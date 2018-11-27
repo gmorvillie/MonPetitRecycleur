5160a490073dc9a2ea4f8e2d96a11c465eb0bbac
guillaume <gui.morvillier@gmail.com>
2018-11-27 13:48:47 +0100

    added resize
    python script for resizing images before label

---
+++ resize.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import os, sys
+
+path = ('./resize/')
+
+dirs = os.listdir( path )
+
+def resize():
+    for item in dirs:
+        
+        
+        #if os.path.isfile(path+item):
+        print("bonn")
+        im = Image.open(path+item)
+        width, height = im.size
+        f, e = os.path.splitext(path+item)
+        imResize = im.resize((int(width/5),int(height/5)), Image.ANTIALIAS)
+        imResize.save(f+'-resized.jpg', 'JPEG', quality=90)
+
+resize()
