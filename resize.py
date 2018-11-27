cff959d2138024e821bcd8ca6c65895c31146278
guillaume <gui.morvillier@gmail.com>
2018-11-27 14:06:48 +0100

    fixed resize

--- resize.py
+++ resize.py
@@ -6,8 +6,9 @@ path = ('./resize/')
 dirs = os.listdir( path )
 
 def resize():
+    i = 0
     for item in dirs:
-        
+        i = i +1
         
         #if os.path.isfile(path+item):
         print("bonn")
@@ -15,6 +16,7 @@ def resize():
         width, height = im.size
         f, e = os.path.splitext(path+item)
         imResize = im.resize((int(width/5),int(height/5)), Image.ANTIALIAS)
-        imResize.save('resized'+f+'.jpg', 'JPEG', quality=90)
+        print("f " +f)
+        imResize.save(path + 'resized-'+str(i)+e, 'JPEG', quality=90)
 
 resize()
