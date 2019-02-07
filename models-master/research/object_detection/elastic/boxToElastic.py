fc41078ae3ae5dec313d6a9b600e6c3a63e65fb1
guillaume <gui.morvillier@gmail.com>
2019-02-07 20:04:03 +0100

    Added Label Images
    execute script and follow graphical interface

--- models-master/research/object_detection/elastic/boxToElastic.py
+++ models-master/research/object_detection/elastic/boxToElastic.py
@@ -5,6 +5,7 @@ from elasticsearch import Elasticsearch
 
 default_address = '18.185.48.125:9200'
 index = {1 : 'grise', 2 : 'verte'}
+
 boxesEX = np.array([[1, 1, 30, 30],[1, 1, 20, 20],[2,1, 20, 20]]) #boxes coordinates
 classesEX = np.array([1, 2, 1]) #orga trash orga
 scoresEX = np.array([0.9, 0.8, 0.9])
@@ -34,4 +35,3 @@ def toElastic(boxes, classes, score, category_index =  index, address = default_
 	
 	
 
-toElastic(boxesEX, classesEX, scoresEX)
--- models-master/research/object_detection/elastic/boxToJson.py
+++ models-master/research/object_detection/elastic/boxToJson.py
@@ -1,6 +1,8 @@
 import json
 import numpy as np
 import datetime
+from elasticsearch import Elasticsearch
+
 
 
 boxesEX = np.array([[1, 2, 1, 2],[1, 3, 1, 3],[2, 3, 2, 3]]) #boxes coordinates
@@ -8,19 +10,28 @@ classesEX = np.array([1, 2, 1]) #orga trash orga
 scoresEX = np.array([0.9, 0.8, 0.9])
 
 def toJson(boxes, classes, score):
-	
+	es = Elasticsearch()
 	with open('box.json', 'w') as outfile: 
 			json.dump([], outfile, indent = 0) 
 	
 	time = str(datetime.datetime.now())
 	
 	for i, elem in enumerate(boxes):
+		box = {
+			'Type de dechet': int(classes[i]),
+			'confidence': float(score[i]),
+			'timestamp': datetime.now(),
+		}
+		#box = {}  
+		#box['Type de dechet'] = int(classes[i])
+		#box['confidence'] = float(score[i])
+		#box['timestamp'] = time
+		res = es.index(index="boites", doc_type='boite', id=1, body=doc)
 		
-		box = {}  
-		box['Type de dechet'] = int(classes[i])
-		box['confidence'] = float(score[i])
-		box['timestamp'] = time
 		
+
+	
+
 		#ICI METTRE DICO.APPEND
 	
 	
